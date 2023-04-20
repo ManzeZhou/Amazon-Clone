13e9141265c1301be7b851028dc4bd36588faf91
Zhou Manze <manzezz1996@gmail.com>
2023-04-20 19:07:42 -0400

    create initial file and add header component

---
+++ amazon-clone/src/firsebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD8wY_OpLRqPy8-eNgJqUvax1MwSWHulrs",
+    authDomain: "clone-v2-e8a68.firebaseapp.com",
+    projectId: "clone-v2-e8a68",
+    storageBucket: "clone-v2-e8a68.appspot.com",
+    messagingSenderId: "311140844687",
+    appId: "1:311140844687:web:b4834bf64870fe645e9cfa",
+    measurementId: "G-R3Y2DQR389"
+};
